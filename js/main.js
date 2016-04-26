9878313b124c39ee1add6765f749ba39858a6006
nimda32 <edmund.dao@sjsu.edu>
2016-04-25 21:55:16 -0700

    Added modal for displaying work, need to check if the modal works on larger and smaller monitors

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
-  $(window).resize(
-    function() {
-      window.location.href = window.location.href;
-    });
+    $(window).resize(
+        function() {
+            window.location.href = window.location.href;
+        });
 
-  $('#mycanvas').click(function(){
-    //show work files
-    window.alert("test");
-  });
+    if (window.location.href.indexOf('#gallery') != -1) {
+        $('#gal').modal('show');
+    }
+
+    // $('#gal').hide();
+    $('#mycanvas').click(function() {
+        $('#gal').modal('show');
+        //pull work from work folder
+
+        // handles closing modal when focus lost
+        $('.modal-backdrop').click(function() {
+            $('#gal').modal('hide');
+        });
+    });
+    //
+
 
 });

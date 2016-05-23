108c5f6247e2442ffc3c1e9883297d685b3904f3
nimda32 <edmund.dao@sjsu.edu>
2016-05-23 00:17:52 -0700

    Fixed Work page popup filter, ready for images and captions

--- js/main.js
+++ js/main.js
@@ -1,29 +1,22 @@
 $(document).ready(function() {
-    $(window).resize(
-        function() {
-            window.location.href = window.location.href;
-        });
-
-    if (window.location.href.indexOf('#gallery') != -1) {
-        $('#gal').modal('show');
-    }
-
-    // $('#gal').hide();
-    $('#mycanvas').click(function() {
-        $('#gal').modal('show');
-        //pull work from work folder
-
-        // // handles closing modal when focus lost
-        // $('.modal-backdrop').click(function() {
-        //     $('#gal').modal('hide');
-        // });
-
-
-
+  $(window).resize(
+    function() {
+      window.location.href = window.location.href;
     });
 
+  if (window.location.href.indexOf('#gallery') != -1) {
+    $('#gal').modal('show');
+  }
 
-    //
+  // $('#gal').hide();
+  $('#mycanvas').click(function() {
+    $('#gal').modal('show');
+    //pull work from work folder
 
+    // // handles closing modal when focus lost
+    $('.modal-backdrop').click(function() {
+      $('#gal').modal('hide');
+    });
 
+  });
 });
